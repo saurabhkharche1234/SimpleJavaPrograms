45255466f4ce823e8d321a536136da6d086d4977
Saurabh <saurabhkharche1234@outlook.com>
2023-08-07 16:41:21 +0530

    Added sorting programs

---
+++ src/sorting/Merge_Sort.java
@@ -0,0 +1,79 @@
+//we sort array using divide and conquer technique where we keep on dividing the array in half and then combine them 
+//si = starting index, ei= ending index
+//time complexity is O(nlogn)
+// to be implemented when we have more memory and even in worst case we want time complexity as O(nlogn)
+package sorting;
+import java.util.Scanner;
+public class Merge_Sort extends Exception {
+
+	public static void conquer(int arr[],int si, int mid, int ei) {   //function to sort
+		int merger[] = new int[ei-si+1];
+		
+		int idx1 = si;
+		int idx2 = mid+1;
+		int x = 0;
+		
+		while(idx1 <=mid && idx2 <=ei) {
+			if(arr[idx1]<=arr[idx2])
+			{
+				merger[x++] = arr[idx1++];
+			}else {
+				merger[x++] = arr[idx2++];
+			}
+		}
+		
+		while(idx1<=mid) {
+			merger[x++] = arr[idx1++];
+		}
+		
+		while(idx2<=ei) {
+			merger[x++] = arr[idx2++];
+		}
+		
+		for(int i=0, j = si; i < merger.length;i++,j++) {
+			arr[j] = merger[i];
+		}
+	}
+	
+	
+	public static void divide(int arr[], int si, int ei) {  // recursive function to divide the array entered 
+		
+		if(si>=ei) {
+			return;
+		}
+		
+		int mid = si + (ei-si)/2;  //to divide in half
+		
+		divide(arr, si, mid);
+		divide(arr,mid+1,ei);
+		conquer(arr,si,mid,ei);
+	}
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			
+			try{
+					System.out.println("Enter the number of elements in the list::");
+					int n = sc.nextInt();
+					int arr[] = new int[n];	
+					System.out.println("Enter the elements::");
+					for(int i=0;i<n;i++)
+						{
+						arr[i] = sc.nextInt();
+						}
+					
+					
+					divide(arr, 0,n-1);   //passed values where si =0 and ei is n-1
+					
+					//print
+					for(int i=0;i<n;i++) {
+						System.out.print(arr[i] +" ");
+						}
+					
+					System.out.println();
+					
+				}catch(Exception e) {
+					System.out.println("Invalid Input Entered.");			
+			}
+		}
+	}
+}
---
+++ src/sorting/QuickSort.java
@@ -0,0 +1,57 @@
+//in quick sort we use pivot and partition technique most preferable way to do quick sort is to set last arrayitem as pivot
+//to be used when we have average cases and we need to use low memory
+//time complexity worst = O(n^2) and average time complexity is O(nlogn)
+//worst case occurs when pivot is always smallest or largest element
+
+package sorting;
+
+public class QuickSort {
+	
+	public static int partition(int arr[],int low, int high) {
+		int pivot = arr[high];
+		int i= low-1;
+		
+		for(int j=low;j<high;j++)
+		{
+			if(arr[j]<pivot) {
+				i++;
+				//swap
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+		i++;
+		int temp = arr[i];
+		arr[i] = pivot;
+		arr[high] =temp;
+		return i;  //pivot
+		
+	}
+	
+	public static void quickSort(int arr[],int low, int high) {
+		
+		if(low<high) {
+			int pidx = partition(arr,low,high);
+			
+			quickSort(arr,low,pidx-1);
+			quickSort(arr,pidx+1,high);
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		int arr[] = {6,3,9,5,2,8};
+		int n = arr.length;
+		
+	quickSort(arr,0,n-1);
+	
+	//print
+		for(int i = 0;i<n;i++)
+		{
+			System.out.print(arr[i]+ "  ");
+		}
+	}
+
+}
